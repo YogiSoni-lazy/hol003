35dd9edeb4eb0de47df05bd6f19c0658d1f26d7b
YogiSoni-lazy <59290335+YogiSoni-lazy@users.noreply.github.com>
2023-10-30 19:18:44 +0530

    Create configure.sh

---
+++ classroom/grading/configure.sh
@@ -0,0 +1,10 @@
+sudo sed -i "s/RHT_COURSE=.*/RHT_COURSE=hol003/" /etc/rht
+sudo sed -i "s/RHT_VERSION_LOCK=.*/RHT_VERSION_LOCK='>=9.0,<10.0'/" /etc/rht
+sudo systemctl restart dynolabs-update.service
+pip install rht-labs-hol003==9.0.2.dev11 --extra-index-url https://pypi.apps.tools-na.prod.nextcle.com/repository/labs/simple/
+lab select hol003
+lab --version
+source ~/.venv/labs/bin/activate
+cd .venv/labs/lib/python3.6/site-packages/hol003/
+#git pull https://github.com/YogiSoni-lazy/hol003.git
+#git clone https://YogiSoni-lazy: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@github.com/YogiSoni-lazy/hol003.git
