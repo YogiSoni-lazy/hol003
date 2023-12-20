e0311a68a1f328df02792cb261029bae275f2024
YogiSoni-lazy <59290335+YogiSoni-lazy@users.noreply.github.com>
2023-12-20 16:51:47 +0530

    Update configure.sh

--- classroom/grading/configure.sh
+++ classroom/grading/configure.sh
@@ -1,7 +1,7 @@
-sudo sed -i "s/RHT_COURSE=.*/RHT_COURSE=hol003/" /etc/rht
+sudo sed -i "s/RHT_COURSE=.*/RHT_COURSE=bfx009/" /etc/rht
 sudo sed -i "s/RHT_VERSION_LOCK=.*/RHT_VERSION_LOCK='>=9.0,<10.0'/" /etc/rht
 sudo systemctl restart dynolabs-update.service
-pip install pip install rht-labs-hol003==9.0.3.dev19 --extra-index-url https://pypi.apps.tools-na.prod.nextcle.com/repository/labs/simple/
+pip install rht-labs-hol003==9.0.8.dev48 --extra-index-url https://pypi.apps.tools-na.prod.nextcle.com/repository/labs/simple/
 lab select hol003
 lab --version
 source ~/.venv/labs/bin/activate
